768be10ca1a4ae9bdba84a6386cd4cf1fb7ad02f
Nour Elsayed <nourelsali2006@gmail.com>
2026-04-07 17:50:53 +0200

    Add files via upload

---
+++ OPPartOne5.dart
@@ -0,0 +1,21 @@
+class calculator {
+  double add(double a, double b) {
+    return a + b;
+  }
+
+  double subtract(double a, double b) {
+    return a - b;
+  }
+
+  double multiply(double a, double b) {
+    return a * b;
+  }
+
+  double divide(double a, double b) {
+    if (b == 0) {
+      print("can't divide by 0");
+      return 0;
+    }
+    return a / b;
+  }
+}
